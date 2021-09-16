10a05deda113a4fb61fc4eb216e4a2c91cfe6b37
kevin210310 <45213740+kevin210310@users.noreply.github.com>
2021-09-15 21:38:17 -0300

    init tcp app

---
+++ index.js
@@ -0,0 +1,32 @@
+const net = require('net');
+
+const server = net.createServer((socket, err) => {
+    
+    socket.on('data', function(data, err){
+
+        let data = Buffer.from(data, 'base64').toString('ascii');
+
+        try {
+
+            const objeto = JSON.parse(data);
+
+            console.log(objeto);
+            
+        } catch (error) {
+
+            console.log("error en el paquete de datos");
+            
+        }
+
+        socket.write(data);
+    });
+
+    socket.on('error', function(err) {
+		console.log(err)
+	})
+
+	socket.on('close', function() {
+		console.log('Connection closed');
+	});
+});
+server.listen(51021, '0.0.0.0');
